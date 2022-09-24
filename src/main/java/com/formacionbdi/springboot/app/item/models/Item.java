198ba6f50bbc56cf8d16b1e4a48da2ed7571878f
Reymon81 <ramon.torres.81@gmail.com>
2022-09-24 03:24:27 +0200

    Initial commit

---
+++ src/main/java/com/formacionbdi/springboot/app/item/models/Item.java
@@ -0,0 +1,26 @@
+package com.formacionbdi.springboot.app.item.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Item {
+
+	private Producto producto;
+	private Integer cantidad;
+	
+	
+	public Item() {	}
+
+	public Item(Producto producto, Integer cantidad) {
+		this.producto = producto;
+		this.cantidad = cantidad;
+	}
+	
+	public Double getTotal() {
+		return producto.getPrecio() * cantidad.doubleValue();
+	}
+
+
+}
